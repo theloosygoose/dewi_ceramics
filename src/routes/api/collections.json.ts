5a8655f94d0d39a62fda0fc746f7fb8105c899f8
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2022-08-18 01:07:53 -0400

    added some SEO stuff

--- src/routes/api/collections.json.ts
+++ src/routes/api/collections.json.ts
@@ -3,8 +3,6 @@ import { getAllCollections } from '$lib/utils/shopify';
 export async function GET() {
     const response = await getAllCollections();
     
-    console.log(response)
-
     return {
         body:{
             collections: await response.body.collections.edges,
