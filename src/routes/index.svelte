b748a8ffc5e0d9778c5dc2c8ca960438bba3dfd7
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2022-08-18 00:26:26 -0400

    added dynamic collections and prefetching on root

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,25 @@
+<script lang="ts" context="module">
+    export async function load({ fetch }) {
+
+        const res = await fetch('/api/collections.json')
+        const collections = await res.json()
+
+        return {
+            props:{
+                collections: await collections.collections
+            }
+        }
+        
+    }
+
+</script>
 <script lang="ts">
     import { fly } from 'svelte/transition';
     import { onMount } from 'svelte';
 
+    export let collections:any 
+    
     let scroll:number;
-
     let ready = false;
     onMount(() => {
         ready = true;
@@ -23,7 +39,7 @@
 </div>
 
 <section class="flex-col">
-    <a href="/store">
+    <a sveltekit:prefetch href="/store">
         <div class="mt-5 lg:bg-store-desktop-img bg-store-mobile-img bg-no-repeat bg-cover bg-center min-h-[500px]">
             <div class="bg-red w-fit">
                 <p class="font-extrabold   py-2 px-6 text-5xl tracking-wide text-tan">store.</p>
@@ -31,26 +47,23 @@
         </div>
     </a>
     <div class="mx-5 mt-5">
-        <h1 class="mx-auto w-full bg-product text-brown font-extrabold xs:text-4xl sm::text-5xl lg:text-7xl text-center tracking-normal underline">COLLECTIONS</h1>
+        <h1 class="mx-auto w-full bg-product text-brown font-extrabold  text-4xl md:text-5xl lg:text-7xl text-center tracking-normal leading-[1]">FEATURED <br> COLLECTIONS</h1>
         <div class="mt-5 grid w-full md:grid-cols-2 grid-cols-1 gap-4">
-            <a href="/store/collections">
-                <div class="lg:bg-maine-collection-desktop-img bg-maine-collection-mobile-img bg-no-repeat bg-cover bg-center min-h-[500px]">
-                    <div class="bg-green w-fit">
-                        <p class="font-extrabold   py-2 px-6 text-5xl tracking-wide text-tan leading-[1] ">made with<br>maine<p>
+        {#each collections as collection, i}
+            <div class="w-full">
+                <a sveltekit:prefetch href="/store/collections/{collection.node.handle}">
+                    <div style="background-image:url('{collection.node.image.transformedSrc}')" class="bg-no-repeat bg-cover bg-top bg-left h-[400px]">
+                        <div class="bg-orange w-fit">
+                            <p class="font-extrabold py-2 px-6 text-5xl tracking-wide text-tan leading-[1]">{collection.node.title}</p>
+                        </div>
                     </div>
-                </div>
-            </a>
-            <a href="/store/collections">
-                <div class="lg:bg-choices-collection-desktop-img bg-choices-collection-mobile-img bg-no-repeat bg-cover bg-left min-h-[500px]">
-                    <div class="bg-orange w-fit">
-                        <p class="font-extrabold   py-2 px-6 text-5xl tracking-wide text-tan leading-[1]">rabbit's <br> picks</p>
-                    </div>
-                </div>
-            </a>
+                </a>
+            </div>
+        {/each}
         </div>
     </div>
     <div>
-        <a href="/about">
+        <a sveltekit:prefetch href="/about">
             <div class="mt-5 lg:bg-mywork-desktop-img bg-mywork-mobile-img bg-no-repeat bg-cover bg-center min-h-[500px]">
                 <div class="bg-blue w-fit">
                     <p class="font-extrabold   py-2 px-6 text-5xl tracking-wide text-tan">about me.</p>
--- src/routes/store/index.svelte
+++ src/routes/store/index.svelte
@@ -1,3 +1,18 @@
+<script lang="ts" context="module">
+
+export async function load({ fetch }){
+    const res = await fetch('/api/collections.json');
+    const collections = await res.json()
+
+
+    return {
+        props: {
+            collections: await collections.collections
+        } 
+    } 
+}
+</script>
+
 <script lang="ts">
     import { fly } from "svelte/transition";
     import { quartOut } from "svelte/easing";
@@ -11,7 +26,7 @@
     <h1 class="text-5xl md:text-6xl lg:text-7xl text-brown font-extrabold tracking-normal text-center underline">STORE</h1>
     <div class="flex-col">
         <div class="bg-red mt-5">
-            <a href="/store/all" class=" md:mx-[15%] grid grid-cols-2 bg-red">
+            <a sveltekit:prefetch href="/store/all" class=" md:mx-[15%] grid grid-cols-2 bg-red">
                 <div in:fly="{{x:-1000, duration:1000, opacity:1, delay:500, easing:quartOut}}" class="h-full grid grid-rows-2 justify-end items-start my-5 mr-[10%] md:mr-[20%] lg:mt-[20%]">
                     <h1 class="text-tan text-2xl sm:text-5xl lg:text-6xl font-extrabold tracking-normal leading-[1]">ALL <br> PRODUCTS</h1>
                     <p class="text-tan text-sm sm:text-lg  font-medium leading-[1]">Click Here to Browse<br>All Products</p>
