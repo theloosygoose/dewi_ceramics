89031ecd2e3b27da4a54cc741d5ef546ecf9de24
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2022-08-24 14:24:28 -0400

    added hover states to the product cards

--- src/lib/components/Cards/ProductCard.svelte
+++ src/lib/components/Cards/ProductCard.svelte
@@ -12,21 +12,30 @@
         return type.name === product.productType
    }) 
 
+   let hoverState = false;
+  async function hoverStateHandler() {
+    hoverState = true;
+  }
+  async function hoverStateHandlerFalse() {
+    hoverState = false;
+  }
 </script>
 
 {#if typeof product != undefined}
-    <a sveltekit:prefetch in:blur="{{delay:1+(i*200),duration:600, easing:cubicInOut}}" class="drop-shadow-sm relative overflow-x-hidden bg-slate-400 aspect-w-1 aspect-h-1 hover:scale-[1.05] transition ease-in-out " href="/store/{product.handle}">
+    <a on:mouseover={hoverStateHandler} on:focus={hoverStateHandler} on:mouseleave={hoverStateHandlerFalse} sveltekit:prefetch in:blur="{{delay:1+(i*200),duration:600, easing:cubicInOut}}" class="drop-shadow-sm relative overflow-x-hidden bg-slate-400 aspect-w-1 aspect-h-1 hover:scale-[1.05] transition ease-in-out " href="/store/{product.handle}">
         <div class="relative z-10">
             <Tag extras="absolute right-0 h-[25px] w-[25px] " text="lg" color={productType[0].color}/>
         </div>
         <div class="h-full">
             <img class="h-full w-full object-cover object-center" loading='lazy' srcset="{product.imageSrc}" alt="{product.handle}">
         </div>
-        <div>
-            <div class="flex-col absolute bottom-0 w-1/2 bg-tan justify-between rounded-tr-sm">
-                <p class="text-brown text-xs md:text-lg lg:text-2xl font-semibold">{product.title}</p>
-                <p class="text-brown text-xs md:text-lg lg:text-2xl font-extrabold tracking-wider">${product.price}0</p>
+        {#if hoverState == false}
+        <div transition:blur={{delay:100, easing:cubicInOut}} class="drop-shadow-2xl">
+            <div class="flex-col absolute bottom-0 w-1/2 bg-tan justify-between">
+                <h4 class="text-brown text-xs md:text-m lg:text-2xl font-semibold leading-6">{product.title}</h4>
+                <p class="text-brown text-xs md:text-m lg:text-2xl font-extrabold tracking-wider">${product.price}0</p>
             </div>
         </div>
+        {/if}
     </a>
 {/if}
